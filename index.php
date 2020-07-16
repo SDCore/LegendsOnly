02189080c13b4ae7834d4cac9e11b2fd906ab70a
SDCore <sdcored@gmail.com>
2020-07-15 19:43:54 -0500

    Initial Commit to Repo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>LegendsOnly - The Hottest Singles in the Outlands</title>
+
+    <link rel="stylesheet" type="text/css" href="./css/main.css" />
+</head>
+
+<body>
+    <img src="imgs/logo/legendsonly_noBackground.png" />
+    <img src="imgs/hero/wraith_lifeline_loba.png" />
+</body>
+
+</html>
